bc2ce0cce9b4d3fea9ce5d5c6dbb408b5346ed7c
Niklas Donges <n.donges@web.de>
2018-06-26 15:03:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'connection96'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'codeberlin'
 
 mysql = MySQL(app)
